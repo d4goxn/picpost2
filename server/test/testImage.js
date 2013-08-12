128f8621be8ad856cd2ef73ad7cd2d0c71c24731
Dan Ross <ross9885@gmail.com>
2013-08-12 13:56:59 -0700

    Testing if GET /images and /image return a posted image.

--- server/test/testImage.js
+++ server/test/testImage.js
@@ -9,6 +9,36 @@ var should = require('should'),
 
 var app = server.start();
 
+function getImageList(done) {
+	request(app)
+	.get('/images')
+	.end(function(error, response) {
+		if(error) return done(error);
+
+		response.should.have.status(200);
+		response.body.images.should.eql({
+			images: ['test image']
+		});
+
+		done();
+	});
+}
+
+function getImage(done) {
+	request(app)
+	.get(encodeURI('/image/test image'))
+	.end(function(error, response) {
+		if(error) return done(error);
+
+		response.should.have.status(200);
+		response.body.images.should.eql({
+			imageUrl: encodeURI('test image.png')
+		});
+
+		done();
+	});
+}
+
 describe('Log', function() {
 	it('should return an empty list when requesting pics from an empty document', function(done) {
 		request(app)
@@ -34,6 +64,35 @@ describe('Log', function() {
 			response.should.have.status(200);
 
 			done();
+		});
+	});
+
+	it('should be able to get a lists of images after posting an image, and get an image from the list', function(done) {
+		request(app)
+		.post('/image')
+		.attach('image', __dirname + '/sampleImage.png')
+		.field('name', 'test pic')
+		.end(function(error, response) {
+			if(error) done(JSON.stringify(error));
+
+			var gotImageList = false;
+			var gotImage = false;
+
+			function checkDone() {
+				if(gotImageList && gotImage) done();
+			}
+
+			getImageList(function(error) {
+				if(error) return done(error);
+				gotImageList = true;
+				checkDone();
+			});
+
+			getImage(function(error) {
+				if(error) return done(error);
+				gotImage = true;
+				checkDone();
+			});
 		});
 	});
 });
